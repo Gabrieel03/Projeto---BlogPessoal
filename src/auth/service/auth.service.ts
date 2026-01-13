0c0c79a6ca6e730ec02b9b86bd9b88e74c7f0c0b
Gabriel <gabriel_.brito@hotmail.com>
2026-01-13 10:35:42 -0300

    Fix: Corrigindo erro

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -50,7 +50,7 @@ export class AuthService {
             usuario: usuarioLogin.usuario,
             senha: '',
             foto: buscaUsuario?.foto,
-            token: `Bearer ${this.jwtService.sign(payload)}`,   // Cria o Token JWT, criptografando alguns dados como o email do usuário que acabou de logar
+            token: this.jwtService.sign(payload),   // Cria o Token JWT, criptografando alguns dados como o email do usuário que acabou de logar
         }
 
     }
